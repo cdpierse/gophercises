66a83c88b3cc24c6c4c477feeee57d18aea00049
Charles Pierse <charlespierse@gmail.com>
2020-06-10 12:16:59 +0100

    Fixed templating bug, temporary

--- cyoa/story.go
+++ cyoa/story.go
@@ -12,29 +12,70 @@ import (
 )
 
 var defaultHandlerTempl = `
-	<!DOCTYPE html>
-	<html>
-
-	<head>
-		<meta charset="utf-8">
-		<title> Choose Your Own Adventure</title>
-	</head>
-
-	<body>
-		<h1>{{.Title}} </h1>
-		{{range .StoryParagraphs}}
-		<p>{{.}}</p>
-		{{end}}
-		<ul>
-			{{range .Options}}
-			<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-			{{end}}
-		</ul>
-
-	</body>
-
-	</html>
-	`
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
+        <p>{{.}}</p>
+      {{end}}
+      {{if .Options}}
+        <ul>
+        {{range .Options}}
+          <li><a href="/">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+      {{else}}
+        <h3>The End</h3>
+      {{end}}
+    </section>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FFFCF6;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #7792a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+  </body>
+</html>`
 
 func init() {
 	tpl = template.Must(template.New("").Parse(defaultHandlerTempl))
@@ -53,7 +94,7 @@ type handler struct {
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	err := tpl.Execute(w, h.s["intro"])
 	if err != nil {
-		panic(err)
+		// panic(err)
 	}
 
 }
@@ -68,16 +109,16 @@ type Story map[string]Chapter
 // a chapter has an array of Option structs which the user interacts with
 // to progress the story.
 type Chapter struct {
-	Title           string   `json:"title"`
-	StoryParagraphs []string `json:"story"`
-	Options         []Option `json:"options"`
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
 }
 
 // Option is a struct that represents the possible options or directions for
 // the story
 type Option struct {
-	Text string `json:"text,omitempty"`
-	Arc  string `json:"arc,omitempty"`
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
 }
 
 func ReadStory(name string) (Story, error) {
@@ -91,6 +132,8 @@ func ReadStory(name string) (Story, error) {
 	if err := d.Decode(&s); err != nil {
 		return nil, err
 	}
+
+	log.Println(s["intro"].Options)
 	return s, nil
 
 }
